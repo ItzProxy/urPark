d3ad1b5a7d232d742e32c8359ed1c05f568c714c
allure.success <allure.success@gmail.com>
2016-03-14 16:12:03 -0400

    Small changes

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 	src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css">
+<link rel="stylesheet" type="text/css" href="css/custom.css">
 <div class="container-fluid">
 	<div class="row">
 		<div class="col-sm-1">
@@ -14,7 +15,7 @@
 			<div class="dropdown">
 				<a id="dLabel" data-target="#" href="#" data-toggle="dropdown"
 					role="button" aria-haspopup="true" aria-expanded="true">
-					Dropdown trigger <span class="caret"></span>
+					Day <span class="caret"></span>
 				</a>
 				<ul class="dropdown-menu" aria-labelledby="dLabel">
 					<li><a href="https://google.ca">Google</a></li>
@@ -25,19 +26,18 @@
 			<div class="dropdown">
 				<a id="dLabel" data-target="#" href="#" data-toggle="dropdown"
 					role="button" aria-haspopup="true" aria-expanded="true">
-					Dropdown trigger <span class="caret"></span>
+					Time <span class="caret"></span>
 				</a>
 				<ul class="dropdown-menu" aria-labelledby="dLabel">
 					<li><a href="https://google.ca">Google</a></li>
 				</ul>
 			</div>
 		</div>
-		<div><span class="glyphicon glyphicon-time"></span></div>
 		<div class="col-sm-2">
 			<div class="dropdown">
 				<a id="dLabel" data-target="#" href="#" data-toggle="dropdown"
 					role="button" aria-haspopup="true" aria-expanded="true">
-					Dropdown trigger <span class="caret"></span>
+					Lot <span class="caret"></span>
 				</a>
 				<ul class="dropdown-menu" aria-labelledby="dLabel">
 					<li><a href="https://google.ca">Google</a></li>
--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php include 'header.php'?>
+<?php include 'side-nav.php'?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<title>Parking Pal</title>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet"
-	href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-<script
-	src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-<script
-	src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	<title>Parking Pal</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
+
 <body>
 	<!--
 <div class="container-fluid">
@@ -20,10 +20,13 @@
   <p>The .container-fluid class provides a full width container, spanning the entire width of the viewport.</p>           
 </div>
 -->
-<!-- Here is where the ajax/jquery request goes to get the map according to the selection
-	 Request or store in cache/cookies is the way to do this -->
-	<div class="well">
-		<img src="Maps/urMap.png" class="img-responsive" alt="urMap">
+	<!-- Here is where the ajax/jquery request goes to get the map according to the selection
+	 Request or store in cache/cookies is the way to do this --> 
+	<div class="mapResize">
+		<div class="well">
+			<img src="Maps/urMap.png" class="img-responsive" alt="urMap">
+		</div>
 	</div>
 </body>
+
 </html>
