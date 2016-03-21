3b05ef270929becb8fdb4c249c351113f693c345
ItzProxy <allure.success@gmail.com>
2016-03-20 22:27:32 -0600

    Used MapBox.js instead of MapboxGL, some changes are still being worked on locally

--- index.php
+++ index.php
@@ -3,25 +3,28 @@
 <html lang="en">
 
 <head>
-	<title>Parking Pal</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<title>Parking Pal</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet"
+	href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<script
+	src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
 
 <body>
 
 	<!-- Here is where the ajax/jquery request goes to get the map according to the selection
-	 Request or store in cache/cookies is the way to do this -->
+	 Request or store in cache/cookies is the way to do this 
 	<div class="overlayMap">
-		<div id="lotx">
-		</div>
+		<div id="lotx"></div>
 	</div>
+	-->
 	<?php include'side-nav.php'?>
 	<div class="map">
-		<?php include 'map.php'?>
+	<?php include 'map.php'?>
 	</div>
 </body>
 
--- map.php
+++ map.php
@@ -4,8 +4,8 @@
     <meta charset='utf-8' />
     <title></title>
     <meta name='viewport' content='initial-scale=1,maximum-scale=1,user-scalable=no' />
-    <script src='https://api.tiles.mapbox.com/mapbox-gl-js/v0.15.0/mapbox-gl.js'></script>
-    <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.15.0/mapbox-gl.css' rel='stylesheet' />
+    <script src='https://api.mapbox.com/mapbox.js/v2.3.0/mapbox.js'></script>
+    <link href='https://api.mapbox.com/mapbox.js/v2.3.0/mapbox.css' rel='stylesheet' />
     <style>
         body { margin:0; padding:0; }
         #map { position:absolute; height:auto; top:2em; bottom:1em; width:80.55%; }
@@ -14,7 +14,8 @@
 <body>
 
 <div id='map'></div>
-<script>
+<pre id='coordinates' class='ui-coordinates'></pre>
+<!--<script>
 mapboxgl.accessToken = 'pk.eyJ1IjoiaXR6cHJveHkiLCJhIjoiY2lsdjhpcDRiMDFoOHVia3NxMXJjejg0NyJ9.BylAtBOCzmlgaLrUk9RarA';
 var bounds = [
     [-104.59709644317627,50.41278399382671], // Southwest coordinates
@@ -27,7 +28,8 @@ var map = new mapboxgl.Map({
     zoom: 5, // starting zoom
     maxBounds: bounds
 });
-</script>
-
+</script> -->
+<script type="text/javascript" src="js/map.js"></script>
+<script type="text/javascript" src="js/getCoord.js"></script>
 </body>
 </html>
