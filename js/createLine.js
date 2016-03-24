1205c55e45d5ac4ae3c37e34198f9bc032b685d1
ItzProxy <allure.success@gmail.com>
2016-03-24 14:05:20 -0600

    Added some admin functions
    Admin testing functions on main page.

--- js/createLine.js
+++ js/createLine.js
@@ -2,13 +2,23 @@
  * Created by Dionne on 3/22/2016.
  */
 var all_drawn_array = [];
-var draw_button = document.getElementById('draw')
+var draw_button = document.getElementById('draw');
+var color_button = document.getElementById('colorOfLine');
 var polyline_options = {
     color: '#000'
 };
 
 draw_button.addEventListener('click', draw_line); //event listener
-
+color_button.addEventListener('click', changeLineColor);//event listener
+function changeLineColor(){ //Changing color of row lines
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    color_button.innerHTML = '' + color + '';
+    polyline_options.color = color;
+}
 function draw_line(){
     var m = marker.getLatLng(),
         n = lenMarker.getLatLng();
