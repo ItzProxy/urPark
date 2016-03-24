14cd78348e3364b3ee604f60101d866dd36afab1
allure.success <allure.success@gmail.com>
2016-03-24 16:53:50 -0400

    added admin view

---
+++ admin.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<title>Parking Pal</title>
+<meta charset="utf-8">
+  
+  </head>
+  <body>
+    
+    <h1> Welcome to the admin page!</h1>
+    <form>
+      <p>Please select which Lot you would like to add data to.</p>
+      <select name="Lots">
+        <option value="Lot3">Lot 3</option>
+        <option value="Lot4">Lot 4</option>
+        <option value="Lot15">Lot 15</option>
+        <option value="Lot17">Lot 17</option>
+        <option value="Lot19">Lot 19</option>
+      </select>
+      <p>Please input the number of cars that were parked in each row.</p>
+      <p>This row has a total of n spaces.</p>
+      <input type="text"/>
+      <input type="submit"/>
+    </form>
+    
+  </body>
+</html>
