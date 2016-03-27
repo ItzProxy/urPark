bff1aedb6720f79aaed68f9f2732f626c6604158
allure.success <allure.success@gmail.com>
2016-03-27 17:19:15 -0400

    admin.php added

--- admin.php
+++ admin.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-<title>Parking Pal</title>
+<title>Parking Pal Admin</title>
 <meta charset="utf-8">
   
   </head>
@@ -10,7 +10,30 @@
     
     <h1> Welcome to the admin page!</h1>
     <form>
-      <p>Please select which Lot you would like to add data to.</p>
+      <p>Please enter your username and password</p>
+      <input type="text" name="username" placeHolder="Username"/>
+      <input type="text" name="password" placeHolder="Password"/>
+      <p>Please enter the name of the map</p>
+      <input type="text" name="map" placeholder="Map"/>
+      <p>Please enter the Northeast and Southwest coordinates of the map</p>
+      <input type="number" name="mapnortheast" placeHolder="Northeast Coordinate" />
+      <input type="number" name="mapsouthwest" placeHolder="Southwest Coordinate" />
+      <p>Please enter the name of the lot</p>
+      <input type="text" name="lot" placeHolder="Lot" />
+      <p>Please enter the Northeast and Southwest coordinates of the lot</p>
+      <input type="number" name="lotnortheast" placeHolder="Northeast Coordinate" />
+      <input type="number" name="lotsouthwest" placeHolder="Southwest Coordinate" />
+      <p>Please enter the total number of rows within the lot</p>
+      <input type="number" name="totalRows" placeHolder="Total Rows" />
+      <p>Please enter the total number of spots within the row</p>
+      <input type="number" name="totalSpots" placeholder="Total Spots" />
+      <p>Please enter the total number of filled spots within that row</p>
+      <input type="number" name="filledSpots" placeholder="Filled Spots"/>
+    </form>
+    
+  </body>
+</html>
+<!--<p>Please select which Lot you would like to add data to.</p>
       <select name="Lots">
         <option value="Lot3">Lot 3</option>
         <option value="Lot4">Lot 4</option>
@@ -21,8 +44,4 @@
       <p>Please input the number of cars that were parked in each row.</p>
       <p>This row has a total of n spaces.</p>
       <input type="text"/>
-      <input type="submit"/>
-    </form>
-    
-  </body>
-</html>
+      <input type="submit"/>-->
