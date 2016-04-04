0595138ccff5f33232b6ff9c5e0af6518e55550e
Mynameisjefflol <johnwyc732@gmail.com>
2016-04-03 19:54:43 -0600

    Added files via upload

---
+++ cufon-replace.js
@@ -0,0 +1,3 @@
+
+Cufon.replace('#menu a, .nivo-caption strong, h2, h3, h4, .date, .call', { fontFamily: 'Didact Gothic', hover:true });
+
---
+++ script.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function() {
+   // initiate tool tip
+	// basic usage  
+	$('.normaltip').aToolTip();  
+	
+	$('#form_1').jqTransform({imgPath:'jqtransformplugin/img/'});	
+ });
